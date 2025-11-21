e9104c548dddd79ac3eda3fc059db1e88b1ded68
sri2101 <sripers23@gmail.com>
2025-11-21 10:31:52 +0530

    debug logs added

--- server/src/routes/package.route.js
+++ server/src/routes/package.route.js
@@ -1,3 +1,57 @@
+// import { Router } from "express";
+// import { verifyUser, verifyAdmin } from "../middlewares/auth.middleware.js";
+// import {
+//   createPackage,
+//   getAllPackages,
+//   getPackagesByLocation,
+//   getPackageBySlug,
+//   updatePackage,
+//   deletePackage,
+// } from "../controllers/package.controller.js";
+// import upload from "../middlewares/multer.middleware.js";
+
+// const router = Router();
+
+// // Public
+// router.get("/all", getAllPackages);
+// router.get("/by-location", getPackagesByLocation);
+// router.get("/:slug", getPackageBySlug);
+
+// // Admin
+// router.post(
+//   "/create",
+//   verifyUser,
+//   verifyAdmin,
+//   upload.fields([
+//     { name: "image1", maxCount: 1 },
+//     { name: "image2", maxCount: 1 },
+//     { name: "image3", maxCount: 1 },
+//     { name: "image4", maxCount: 1 },
+//     { name: "image5", maxCount: 1 },
+//   ]),
+//   createPackage
+// );
+
+// router.put(
+//   "/:slug",
+//   verifyUser,
+//   verifyAdmin,
+//   upload.fields([
+//     { name: "image1", maxCount: 1 },
+//     { name: "image2", maxCount: 1 },
+//     { name: "image3", maxCount: 1 },
+//     { name: "image4", maxCount: 1 },
+//     { name: "image5", maxCount: 1 },
+//   ]),
+//   updatePackage
+// );
+// router.delete("/:slug", verifyUser, verifyAdmin, deletePackage);
+
+// export default router;
+
+
+
+
 import { Router } from "express";
 import { verifyUser, verifyAdmin } from "../middlewares/auth.middleware.js";
 import {
@@ -29,6 +83,11 @@ router.post(
     { name: "image4", maxCount: 1 },
     { name: "image5", maxCount: 1 },
   ]),
+  (req, res, next) => {
+    console.log("BODY:", req.body);
+    console.log("FILES:", req.files);
+    next(); // Continue to createPackage controller
+  },
   createPackage
 );
 
@@ -43,8 +102,14 @@ router.put(
     { name: "image4", maxCount: 1 },
     { name: "image5", maxCount: 1 },
   ]),
+  (req, res, next) => {
+    console.log("BODY:", req.body);
+    console.log("FILES:", req.files);
+    next(); // Continue to updatePackage controller
+  },
   updatePackage
 );
+
 router.delete("/:slug", verifyUser, verifyAdmin, deletePackage);
 
 export default router;
